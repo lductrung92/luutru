a511ea2301d6a639f86283fa827798b6839be9c5
Trung Lee <lductrung92@gmail.com>
2017-04-26 16:57:39 +0700

    DIVE

--- laravel/bai5.blade.php
+++ laravel/bai5.blade.php
@@ -9,14 +9,76 @@
 </div>
 <div id="category_01">
     <p><strong>1. Tạo Blade template engine (BTE)</strong></p>
+    <p>Tất cả các Blade template engine (BTE) đều có phần mở rộng là <code>.blade.php</code>, nằm trong thư mục <code>resources/views</code>. Việc tạo mới BTE rất đơn giản. Ví dụ để tạo BTE tên là <code>demo</code> ta tạo file <code>demo.blade.php</code> nằm trong thư mục <code>resources/views</code> là xong.</p>
 </div>
 <div id="category_02">
     <p><strong>2. Cú pháp trong BTE</strong></p>
+    <p><strong>BTE sử dụng cặp ngoặc <code>{{}}</code> để echo giá trị (in ra định dạng html)</strong></p>
+    <p>Ví dụ:</p>
+    <pre class="brush: php">
+        Ngày hôm nay: {{ date("d/m/y") }}
+        {{ '<p>Hôm nay trời đep thế</p>' }}
+    </pre>
+    <p>Kết quả: </p>
+    <pre class="brush: php">
+    Ng&agrave;y h&ocirc;m nay: 26/04/17&lt;p&gt;H&ocirc;m nay trời đep thế&lt;/p&gt;
+    </pre>
+
+    <p><strong>BTE sử dụng <code>{!! !!}</code> để echo giá trị (đọc định dạng html rồi in ra)</strong></p>
+    <p>Ví dụ:</p>
+    <pre class="brush: php">
+       Ngày hôm nay: {{ date("d/m/y") }}
+        {!! '<p>Hôm nay trời đep thế</p>' !!}
+    </pre>
+    <p>Kết quả: </p>
+    <pre class="brush: php">
+        Kết quả: Ngày hôm nay: 26/04/17
+        Hôm nay trời đep thế
+    </pre>
+    <p><strong>Sử dụng <code>or</code> để xuất giá trị mặc định</strong></p>
+    <p>Ví dụ: </p>
+    <pre class="brush: php">
+    Xin chào, {{ $name or 'TrungLe' }}
+    </pre>
+    <p><code>$name<code> này không tồn tại thì kết quả sẽ mặc định là TrungLe</code></code></p>
+
 </div>
 <div id="category_03">
     <p><strong>3. Chuẩn bị giao diện cho trang quản trị(Admin-LTE)</strong></p>
-</div>
+    <p>Bài hôm nay nay chúng ta khởi động làm 1 website tin tức luôn nhé.</p>
+    <p>Tạo project mới: </p>
+    <pre class="brush: php">
+        $ composer create-project --prefer-dist laravel/laravel tintuc
+    </pre>
+    <p>Download AdminLTE: <a href="https://github.com/almasaeed2010/AdminLTE/archive/master.zip" target="_blank">tại đây</a></p>
+    <p>Giải nén vào thư mục <code>public</code> đổi tên thành adminlte(tùy)</p>
+    <p>Tạo view như sau: </p>
+    <p style="text-align: center"><img alt="" src="http://laptrinh.laedaily.com/laravel-filemanager/photos/1/laravel/bai5/1.jpg"/></p>
+    <p>Nội dung từng file: </p>
+    <p>Code hơi dài nên các bạn có thể xem video hướng dẫn và download: <a href="http://laptrinh.laedaily.com/laravel-filemanager/photos/1/laravel/bai5/views.zip" target="_blank">tại đây</a></p>
+    <p>Tạo<code>Route</code></p>
+    <pre class="brush: php">
+        Route::group(['prefix' => 'administrator'], function () {
+           Route::get('index', 'AdminController@index');
+        });
+    </pre>
+    <p>Nội dung controller <code>AdminController</code></p>
+    <pre class="brush: php">
+        class AdminController extends Controller
+        {
+            public function index ()
+            {
+                return view('admin.index');
+            }
+        }
+    </pre>
+    <p>Các bạn truy cập vào: http://localhost/tintuc/public/administrator/index </p>
+    <p>Kết quả</p>
+    <p style="text-align: center"><img alt="" src="http://laptrinh.laedaily.com/laravel-filemanager/photos/1/laravel/bai5/2.jpg"/></p>
 
+</div>
+<p><strong>Video hướng dẫn: </strong></p>
+<p>Trong bài này mình đã giới thiệu về <code>Blade Template Engine</code> trong Laravel 5. Trong series bài viết tiếp theo mình sẽ hướng dẫn các bạn về <code>Migrations</code> trong Laravel 5. Mời các bạn đón đọc. Bài viết trên là những hiểu biết của cá nhân mình nên không tránh phải sai sót, rất mong sự đóng góp ý kiến của các bạn để bài viết trở nên hữu ích hơn.</p>
 
 
 
--- laravel/bai6.blade.php
+++ laravel/bai6.blade.php
@@ -5,25 +5,125 @@
         <li><a id = "category_01_link" href="#">Cấu hình database</a></li>
         <li>
             <a id = "category_02_link" href="#">Sử dụng migrations trong laravel 5</a>
-            <ul>
-                <li><a id = "category_03_link" href="#">Tạo bảng bằng migrations</a></li>
-                <li><a id = "category_04_link" href="#">Cập nhật bảng bằng migrations</a></li>
-            </ul>
         </li>
-        <li><a id = "category_05_link" href="#">Tạo database website tin tức cơ bản bằng migrations</a></li>
+        <li><a id = "category_03_link" href="#">Tạo database website tin tức cơ bản bằng migrations</a></li>
     </ul>
 </div>
 <div id="category_01">
     <p><strong>1. Cấu hình database</strong></p>
+    <p>Để cấu hình database trong Laravel, bạn tìm đến file <code>.env</code> ở thu mục gốc của ứng dụng.</p>
+    <p style="text-align: center"><img alt="" src="http://laptrinh.laedaily.com/laravel-filemanager/photos/1/laravel/bai6/1.jpg"/></p>
+    <p>Mở file lên với editor bất kỳ, tìm đến và chỉnh sửa 6 dòng như trong hình (trong trường hợp này mình dùng mysql). Tùy server, tên database, tài khoản của bạn để điền tương ứng</p>
+    <p style="text-align: center"><img alt="" src="http://laptrinh.laedaily.com/laravel-filemanager/photos/1/laravel/bai6/2.jpg"/></p>
 </div>
 <div id="category_02">
     <p><strong>2. Sử dụng migrations trong laravel 5</strong></p>
-</div>
-<div id="category_03">
-    <p><strong>Tạo bảng bằng migrations</strong></p>
-</div>
-<div id="category_03">
-    <p><strong>Cập nhật bảng bằng migrations</strong></p>
+    <p><code>Migration</code> cơ sở dữ liệu trong Laravel cung cấp cho bạn một cách dễ dàng việc quản lý những schema CSDL trong ứng dụng. Khi bạn sử dụng migration, nhiều tác vụ liên quan tới CSDL schema dễ dàng hơn rất nhiều; bằng cách sử dụng migration bạn có thể thay đổi cấu trúc của bảng cơ sở dữ liệu, bạn có thể giữ lại những dữ liệu cũ bất cứ khi nào cho dù hiện tại bạn có thay đổi.</p>
+    <p>Tạo bảng bằng <code>Migration</code></p>
+    <pre class="brush: php">
+        $ php artisan make:migration created_table_name --create=name
+    </pre>
+    <p>Chèn thêm columns vào bảng đã có sẵn <code>Migration</code></p>
+    <pre class="brush: php">
+        $ php artisan make:migration add_columns_table_name --table=name
+    </pre>
+    <p>Kết quả tạo ra file trong thư mục <code>database/migrations</code></p>
+    <p>Ví dụ: </p>
+    <p>Khi chạy lệnh khởi tạo như sau: </p>
+    <pre class="brush: php">
+        $ php artisan make:migration create_table_categories --create=categories
+    </pre>
+    <p>Trong thư mục <code>database/migrations</code> sẽ tạo cho bạn file <code>2017_04_26_074546_create_table_categories.php</code></p>
+    <p>Nội dung file: </p>
+    <pre class="brush: php">
+        // database/migrations
+        use Illuminate\Support\Facades\Schema;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Database\Migrations\Migration;
+
+        class CreateTableCategories extends Migration
+        {
+            /**
+             * Run the migrations.
+             *
+             * @return void
+             */
+            public function up()
+            {
+                Schema::create('categories', function (Blueprint $table) {
+                    $table->increments('id');
+                    $table->timestamps();
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists('categories');
+            }
+        }
+    </pre>
+    <p>Dưới đây là một số câu lệnh của những hàm liên quan tới Schema mà Laravel cung cấp</p>
+    <table>
+        <thead>
+        <tr>
+            <th>Câu lệnh</th>
+            <th>Diễn giãi</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="active"><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;increments(‘id’);<br>
+                </span></td>
+            <td class="success">Tạo khóa ID tự động tăng trong bảng</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;string(’email’);</span></td>
+            <td>Trường dữ liệu khai báo với kiểu VARCHAR</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;string(‘name’, 100);</span></td>
+            <td>Trường dữ liệu khai báo với kiểu VARCHAR với số kí tự 100</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;integer(‘votes’);</span></td>
+            <td>Trường dữ liệu khai báo với kiểu INTEGER</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;float(‘amount’);</span></td>
+            <td>Trường dữ liệu khai báo với kiểu FLOAT</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;boolean(‘confirmed’);</span></td>
+            <td>Trường dữ liệu khai báo với kiểu BOOLEAN</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;timestamps();</span></td>
+            <td>Tự động thêm trường created_at, updated_at</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">$table-&gt;text(‘description’);</span></td>
+            <td>Trường dữ liệu khai báo với kiểu TEXT</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">-&gt;nullable()</span></td>
+            <td>Trường dữ liệu có thể ghi dữ liệu null</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">-&gt;default($value)</span></td>
+            <td>Đặt giá trị mặc định cho trường</td>
+        </tr>
+        <tr>
+            <td><span style="color: #bd3613; font-family: Menlo, Monaco, Consolas, 'Courier New', monospace; font-size: 13px; font-style: normal; font-variant: normal; font-weight: normal; letter-spacing: normal; line-height: 18px;  text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; -webkit-text-stroke-width: 0px; display: inline !important; float: none; background-color: #fdf6e3;">-&gt;unsigned()</span></td>
+            <td>Đặt trường dữ liệu là số nguyên dương</td>
+        </tr>
+        </tbody>
+    </table>
+    <p>Xem chi tiết: <a href="https://laravel.com/docs/5.4/migrations" target="_blank">tại đây</a></p>
 </div>
 <div id="category_03">
     <p><strong>3. Tạo database website tin tức cơ bản bằng migrations</strong></p>
